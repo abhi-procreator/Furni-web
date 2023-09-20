5790c900c70cc4c4fe391de52394088d0b514734
abhi-procreator <hi2abhi.shrm@gmail.com>
2023-09-20 23:58:22 +0530

    first commit

---
+++ script.js
@@ -0,0 +1,10 @@
+//   import:'./style.css'
+
+$('#slider').slick({
+    dots: true,
+    infinite: true,
+    // autoplay: true,
+    // autoplaySpeed: 2500,
+    speed: 300,
+    slidesToShow: 1,
+  });
